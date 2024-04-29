5b505da53b4cba0528c2640f10ead165bed22ed4
kshlove735 <kshlove735@gmail.com>
2024-04-30 01:13:18 +0900

    클래스, 멤버변수, 메소드

---
+++ src/chapter5/StudenTest.java
@@ -0,0 +1,15 @@
+package chapter5;
+
+public class StudenTest {
+
+	public static void main(String[] args) {
+
+		Student studentLee = new Student();
+		studentLee.studentName = "이순신";
+		studentLee.address = "서울시 서초동";
+		
+		studentLee.showStudentInfor();
+
+	}
+
+}
