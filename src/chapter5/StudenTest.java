d96da761b4b905334b46b9aa025b980ddeda444a
kshlove735 <kshlove735@gmail.com>
2024-05-29 00:48:15 +0900

    생성자

--- src/chapter5/StudenTest.java
+++ src/chapter5/StudenTest.java
@@ -3,13 +3,18 @@ package chapter5;
 public class StudenTest {
 
 	public static void main(String[] args) {
-
-		Student studentLee = new Student();
-		studentLee.studentName = "이순신";
-		studentLee.address = "서울시 서초동";
+		
+		Student studentLee = new Student(100, "이순신");
+		studentLee.address= "서울시 영등포구 여의도동";
+		
+		Student studentKim = new Student();
+		studentKim.studentName = "김유신";
+		studentKim.studentID=101;
+		studentKim.address= "미국 산호세";
 		
 		studentLee.showStudentInfor();
-
+		studentKim.showStudentInfor();
+		
 	}
 
 }
--- src/chapter5/Student.java
+++ src/chapter5/Student.java
@@ -2,11 +2,21 @@ package chapter5;
 
 public class Student {
 
+	// 멤버 변수
 	int studentID;
 	String studentName;
 	int grade;
 	String address; 
 	
+	//	default 생성자 : 작성 안해도 컴파일러가 생성해줌. 단! 다른 생성자가 없을때만 자동 생성 
+	public Student() {}
+	
+	public Student(int id, String name) {
+		this.studentID = id;
+		this.studentName = name;
+	}
+	
+	// 메서드
 	public void showStudentInfor() {
 		System.out.println(studentName + ", " + address);
 	}
@@ -19,4 +29,21 @@ public class Student {
 	public void setStudentName(String name) {
 		studentName = name;
 	}
+	
+	public static void main(String[] args) {
+		
+		Student studentLee = new Student(100, "이순신");
+		studentLee.address= "서울시 영등포구 여의도동";
+		
+		Student studentKim = new Student();
+		studentKim.studentName = "김유신";
+		studentKim.studentID=101;
+		studentKim.address= "미국 산호세";
+		
+		studentLee.showStudentInfor();
+		studentKim.showStudentInfor();
+		
+	}
+	
+	
 }
